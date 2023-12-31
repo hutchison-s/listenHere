3b820049790c4cd5d6ce8691a7721e44da77f15e
Steven Hutchison <hutchison.music@gmail.com>
2023-12-31 01:21:33 -0600

    Add leaflet integration

--- root/src/App.jsx
+++ root/src/App.jsx
@@ -1,11 +1,40 @@
 import './App.css'
 import NavBar from "./components/NavBar"
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import { useState, useEffect } from 'react';
+import { MapContainer, TileLayer} from 'react-leaflet';
+// later add TileLayer, Marker, Popup 
 
 function App() {
+  useEffect(() => {
+    const handleBeforeUnload = (event) => {
+      navigator.geolocation.clearWatch(watcher)
+    };
+    window.addEventListener('beforeunload', handleBeforeUnload);
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, []);
+
+  const [currentLocation, setCurrentLocation] = useState(null)
+  function onFound({coords}) {
+    setCurrentLocation(coords)
+  }
+  function onError(err) {
+    console.log(err)
+  }
+  const watcher = navigator.geolocation.watchPosition(onFound, onError)
 
   return (
     <>
       <NavBar/>
+      {currentLocation && (<MapContainer center={[currentLocation.latitude, currentLocation.longitude]} zoom={13} style={{ height: '400px' }}>
+        {<TileLayer
+  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+/>}
+    </MapContainer>)}
     </>
   )
 }
