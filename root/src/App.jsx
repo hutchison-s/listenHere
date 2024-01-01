eb978667c1cc1e7cbe335a4de35f062469453e7f
Steven Hutchison <hutchison.music@gmail.com>
2023-12-31 23:25:38 -0600

    Create icons, switch map styling, add markers

--- root/src/App.jsx
+++ root/src/App.jsx
@@ -4,7 +4,8 @@ import NavBar from "./components/NavBar"
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import { useState, useEffect } from 'react';
-import { MapContainer, TileLayer} from 'react-leaflet';
+import { MapContainer, TileLayer, Marker} from 'react-leaflet';
+import { Icon } from 'leaflet';
 // later add TileLayer, Marker, Popup 
 
 function App() {
@@ -19,23 +20,49 @@ function App() {
   }, []);
 
   const [currentLocation, setCurrentLocation] = useState(null)
+
+  const youPin = new Icon({
+    iconUrl: "./src/assets/person-rays-solid.svg",
+    iconSize: [40, 40]
+  });
+
+  const earPin = new Icon({
+    iconUrl: "/earpin.png",
+    iconSize: [50, 50]
+  });
+
+  const pins = [
+    {user: 1234, lat: 40.7378845, lng: -96.638, msg: "Hello There"},
+    {user: 1235, lat: 40.7376, lng: -96.640, msg: "Hello There"},
+    {user: 1236, lat: 40.73717, lng: -96.6503, msg: "Hello There"},
+    {user: 1237, lat: 40.73783, lng: -96.6429, msg: "Hello There"},
+  ]
+
   function onFound({coords}) {
     setCurrentLocation(coords)
+    console.log(coords.latitude, coords.longitude)
   }
   function onError(err) {
     console.log(err)
   }
   const watcher = navigator.geolocation.watchPosition(onFound, onError)
+  const latlng = ()=>[currentLocation.latitude, currentLocation.longitude]
 
   return (
     <>
       <NavBar/>
-      {currentLocation && (<MapContainer center={[currentLocation.latitude, currentLocation.longitude]} zoom={13} style={{ height: '400px' }}>
-        {<TileLayer
-  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-/>}
-    </MapContainer>)}
+      {currentLocation && (
+        <MapContainer center={latlng()} zoom={16}>
+          <Marker position={latlng()} icon={youPin}/>
+          {pins.map(pin => (
+            <Marker position={[pin.lat, pin.lng]} icon={earPin} key={pin.user}/>
+          ))}
+          {<TileLayer
+            url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+          />}
+        </MapContainer>
+      )}
     </>
   )
 }
