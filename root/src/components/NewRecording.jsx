49e4daacf9d4f55c7edc38df21ab4b8ffcabb725
Steven Hutchison <hutchison.music@gmail.com>
2024-01-01 16:17:37 -0600

    Add recording ability

--- root/src/components/NewRecording.jsx
+++ root/src/components/NewRecording.jsx
@@ -1,15 +1,70 @@
 import "./NewRecording.css"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMicrophone } from '@fortawesome/free-solid-svg-icons'
+import { faMicrophone, faStop } from '@fortawesome/free-solid-svg-icons'
 
 export default function NewRecording() {
     const [isExpanded, setIsExpanded] = useState(false)
+    const [tempRecording, setTempRecording] = useState(null)
+    const [tempBlob, setTempBlob] = useState(null)
+    const [isRecording, setIsRecording] = useState(false)
+    const [streamer, setStreamer] = useState(null)
+    const chunks = [];
+
+    useEffect(()=>{
+        initiateStream()
+        console.log("initiated")
+    }, [])
+
+    function initiateStream() {
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices.getUserMedia({audio: true})
+                .then((stream) => {
+                    const mediaRecorder = new MediaRecorder(stream)
+                    mediaRecorder.ondataavailable = (e)=>{chunks.push(e.data)}
+                    mediaRecorder.onstop = ()=>{
+                        const blob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
+                        setTempBlob(blob)
+                        chunks.length = 0;
+                        const audioURL = window.URL.createObjectURL(blob);
+                        setTempRecording(audioURL)
+                    }
+                    setStreamer(mediaRecorder)
+                    console.log(mediaRecorder)
+                })
+                .catch((err) => {
+                    console.error(`The following getUserMedia error occurred: ${err}`);
+                });
+        } else {
+            console.log("getUserMedia not supported on your browser!");
+        }
+    }
+    
+
+    function record() {
+        console.log(streamer)
+            if (isRecording) {
+                streamer.stop()
+                setIsRecording(false)
+            } else {
+                streamer.start()
+                setIsRecording(true)
+            }
+    }
+
     function RecordingInterface() {
         return (
-            <div id="recordingInterface" className={isExpanded ? "expanded" : ""}>
-                <h2>Start Recording</h2>
-                <button><FontAwesomeIcon icon={faMicrophone}/></button>
+            <div id="recordingInterface" className={isExpanded ? "expanded" : "collapsed"}>
+                <h2>New Drop</h2>
+                {tempRecording && <audio src={tempRecording} controls></audio>}
+                {tempRecording
+                    ? (<div id="postRecording">
+                        <button id="dropRecording" onClick={()=>{console.log(tempBlob)}}>DROP</button>
+                        <button id="deleteRecording" onClick={()=>{setTempRecording(null); setTempBlob(null)}}>DELETE</button>
+                      </div>)
+                    : (<button id="recordStart" onClick={record} className={isRecording ? "recording" : ""}>
+                    {isRecording ? <FontAwesomeIcon icon={faStop}/> : <FontAwesomeIcon icon={faMicrophone}/>}
+                </button>)}
             </div>
         )
     }
@@ -17,9 +72,11 @@ export default function NewRecording() {
     return (
         <>
             <RecordingInterface/>
-            <button 
+             <button 
                 id="dropNew"
-                onClick={()=>{setIsExpanded(!isExpanded)}}
+                onClick={()=>{
+                    setIsExpanded(!isExpanded)
+                }}
             ></button>
         </>
     )
