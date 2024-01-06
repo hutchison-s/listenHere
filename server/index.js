36a23704a615182534e1bf2495061960253b777f
Steven Hutchison <hutchison.music@gmail.com>
2024-01-06 01:08:23 -0600

    Remove half-typed import

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors')
-const bodyParser = require('bodyParses')
 const mongoose = require('mongoose');
 const EarPin = require('./schemas/EarPin')
 const User = require('./schemas/User')
